9a938cc748af4aa44f3dc80f86ae94bbde46f44f
Tianmu Li <tianmu.li@intel.com>
2025-01-24 02:29:12 +0200

    Add secondary mlp override value

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -132,6 +132,8 @@ class LlamaMLP(nn.Module):
         seq_len = x.size(1)
         if (seq_len*batch_size)%VLLM_MLP_SIZE_OVERRIDE==0:
             x = x.view(-1,VLLM_MLP_SIZE_OVERRIDE,self.hidden_size)
+        elif (seq_len*batch_size)%VLLM_MLP_SIZE_OVERRIDE_2==0:
+            x = x.view(-1,VLLM_MLP_SIZE_OVERRIDE_2,self.hidden_size)
         if self.split_gate_up:
             x = nn.functional.silu(self.gate_proj(x)[0]) * self.up_proj(x)[0]
         else:
@@ -141,7 +143,7 @@ class LlamaMLP(nn.Module):
         # Separate split for down is not implemented yet
         x, _ = self.down_proj(x)
 
-        if (seq_len*batch_size)%VLLM_MLP_SIZE_OVERRIDE==0:
+        if ((seq_len*batch_size)%VLLM_MLP_SIZE_OVERRIDE==0) or ((seq_len*batch_size)%VLLM_MLP_SIZE_OVERRIDE_2==0):
             x = x.view(batch_size,seq_len,self.hidden_size)
         return x
 
